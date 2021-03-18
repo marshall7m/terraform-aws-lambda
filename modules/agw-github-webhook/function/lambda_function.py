fe59eb5f158eaf378914ee6df6fa94b93a1e9658
marshall7m <44485531+marshall7m@users.noreply.github.com>
2021-03-18 10:47:32 -0700

    rm invoke child func logic from lambda func

--- modules/agw-github-webhook/function/lambda_function.py
+++ modules/agw-github-webhook/function/lambda_function.py
@@ -8,7 +8,6 @@ import re
 
 log = logging.getLogger(__name__)
 ssm = boto3.client('ssm')
-lmbda = boto3.client('lambda')
 def lambda_handler(event, context):
     
     authorized = validate_sig(event['headers']['X-Hub-Signature-256'], event['body'])
@@ -26,19 +25,10 @@ def lambda_handler(event, context):
             "body": json.dumps({'error': 'payload can not be decoded'})
         }
 
-    if 'CHILD_LAMBDA_ARN' in os.environ:
-        log.info('invoking child Lambda function: {}'.format(os.environ['CHILD_LAMBDA_ARN']))
-        response = lmbda.invoke(
-            FunctionName = os.environ['CHILD_LAMBDA_ARN'],
-            InvocationType = 'RequestResponse',
-            Payload = parsed_payload
-        )
-        return response
-    else:
-        return {
-            'statusCode': 200,
-            'body': json.dumps('Request is valid')
-        }
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Request is valid')
+    }
 
 def validate_sig(header_sig, payload):
     github_secret = ssm.get_parameter(Name=os.environ['GITHUB_WEBHOOK_SECRET_SSM_KEY'], WithDecryption=True)['Parameter']['Value']
@@ -53,9 +43,9 @@ def validate_sig(header_sig, payload):
         return False
 
     expected_sig = hmac.new(bytes(github_secret, 'utf-8'), bytes(payload, 'utf-8'), hashlib.sha256).hexdigest()
-    
+
     authorized = hmac.compare_digest(str(sig), str(expected_sig))
-    
+
     if not authorized:
        log.error('Header signature and expected signature do not match')
     
